89550feacbd488dd116c232f766f8bf38569755a
Hagop Tutundjian <hagoptutundjian@users.noreply.github.com>
2026-01-30 20:57:03 -0800

    Create 0002_long_nextwave.sql

---
+++ drizzle/migrations/0002_long_nextwave.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "public"."acquisition_type" ADD VALUE 'free_agent_auction' BEFORE 'faab';--> statement-breakpoint
+ALTER TYPE "public"."acquisition_type" ADD VALUE 'waiver_wire' BEFORE 'faab';
